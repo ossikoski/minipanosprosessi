c5f4f6619ea28538b4b246642fae732fc5e08226
ossikoski <ossioskari@gmail.com>
2022-12-05 16:12:26 +0200

    Lisätty indicators ja niiden käyttö UpdateProcessItemsissä. Impregnation vaihe toteutettu yksittäiseen metodiin.

--- Minipanosprosessi/Minipanosprosessi/ControlSystem.cs
+++ Minipanosprosessi/Minipanosprosessi/ControlSystem.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Tuni.MppOpcUaClientLib;
 
@@ -16,37 +17,43 @@ namespace Minipanosprosessi
         /// <summary>
         /// Mikä prosessin vaihe on meneillään
         /// </summary>
-        impregnation_1,
-        impregnation_2,
-        impregnation_3,
-        impregnation_4,
-        impregnation_5,
-        black_liquor_fill_1,
-        black_liquor_fill_2,
-        white_liquor_fill_1,
-        white_liquor_fill_2,
-        cooking_1,
-        cooking_2,
-        cooking_3,
-        cooking_4,
-        cooking_5,
-        cooking_6,
-        cooking_7,
-        discharge_1,
-        discharge_2
+        impregnation,
+        black_liquor_fill,
+        white_liquor_fill,
+        cooking,
+        discharge
     }
+    public struct Indicators
+    {
+        // alkuarvot (20, 20, 0, 216, 90, 90, 0, false, true, true, true)
+        public double TI100;
+        public double TI300;
+        public double FI100;
+        public int LI100;
+        public int LI200;
+        public int LI400;
+        public int PI300;
+        public bool LSp300;  // LSp300 = LS+300
+        public bool LSm300;  // LSm300 = LS-300
+        public bool LSm200;
+        public bool LA100;  // false -> alarm is active and level is under 100)
+    }
+    
+    // TODO siirrä public struct Settings tänne ja alusta rakentajassa
     class ControlSystem : IProcessObserver
     {
         Communication communicationObject;
         bool isStarted;
         Stage stage;
+        MainWindow.Settings settingsObject;
+        Indicators indicators;
         public ControlSystem(Communication communication)
         {
             communicationObject = communication;
             isStarted = false;
-            stage = Stage.impregnation_1;
+            stage = Stage.impregnation;
+            indicators = new Indicators();
         }
-
         public void Start()
         {
             isStarted = true;
@@ -60,7 +67,7 @@ namespace Minipanosprosessi
 
         public void UpdateSettings(MainWindow.Settings settings)
         {
-
+            settingsObject = settings;
         }
 
         public void UpdateConnectionStatus(ConnectionStatusEventArgs args)
@@ -68,34 +75,134 @@ namespace Minipanosprosessi
             //throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Update process items, in this case, those are indicator values
+        /// </summary>
+        /// <param name="args"></param>
         public void UpdateProcessItems(ProcessItemChangedEventArgs args)
         {
-            //throw new NotImplementedException();
+            foreach (var item in args.ChangedItems)
+            {
+                switch (item.Key)
+                {
+                    case "TI100":
+                        indicators.TI100 = (double)item.Value.GetValue();
+                        break;
+                    case "TI300":
+                        indicators.TI300 = (double)item.Value.GetValue();
+                        break;
+                    case "FI100":
+                        indicators.FI100 = (double)item.Value.GetValue();
+                        break;
+                    case "LI100":
+                        indicators.LI100 = (int)item.Value.GetValue();
+                        break;
+                    case "LI200":
+                        indicators.LI200 = (int)item.Value.GetValue();
+                        break;
+                    case "LI400":
+                        indicators.LI400 = (int)item.Value.GetValue();
+                        break;
+                    case "PI300":
+                        indicators.PI300 = (int)item.Value.GetValue();
+                        break;
+                    case "LS+300":
+                        indicators.LSp300 = (bool)item.Value.GetValue();
+                        break;
+                    case "LS-300":
+                        indicators.LSm300 = (bool)item.Value.GetValue();
+                        break;
+                    case "LS-200":
+                        indicators.LSm200 = (bool)item.Value.GetValue();
+                        break;
+                    case "LA100":
+                        indicators.LA100 = (bool)item.Value.GetValue();
+                        break;
+                    default:
+                        // TODO throw exception ? 
+                        break;
+                }
+            }
         }
 
         private void RunLoop()
         {
-            Impregnation1();
-            /*
             while (isStarted){
                 switch (stage)
                 {
-                    case Stage.impregnation_1:
-                        Impregnation1();
-                    case Stage.impregnation_2:
-                        Impregnation2();
+                    case Stage.impregnation:
+                        Impregnation();
+                        break;
+                    case Stage.black_liquor_fill:
+                        BlackLiquorFill();
+                        break;
+                    case Stage.white_liquor_fill:
+                        WhiteLiquorFill();
+                        break;
+                    case Stage.cooking:
+                        Cooking();
+                        break;
+                    case Stage.discharge:
+                        Discharge();
+                        break;
+                    default:
+                        break;
                 }
+                stage++;
             }
-            */
         }
 
-        private void Impregnation1()
+        private void Impregnation()
         {
+            // Phase 1:
             communicationObject.setItem("V201", true);
             communicationObject.setItem("V303", true);
             communicationObject.setItem("P200", 100);
             communicationObject.setItem("V204", true);
             communicationObject.setItem("V301", true);
+
+            while (!indicators.LSp300){}  // TODO: Failsafe?, miten toimii säikeiden kanssa?
+
+            // Phase 2:
+            communicationObject.setItem("V104", false);
+            communicationObject.setItem("V204", false);
+            communicationObject.setItem("V401", false);
+            Thread.Sleep(5000);  // TODO: Sleep(Ti), asetuksista
+
+            // Phase 3
+            communicationObject.setItem("V201", false);
+            communicationObject.setItem("V303", false);
+            communicationObject.setItem("P200", 0);
+            communicationObject.setItem("V104", false);
+            communicationObject.setItem("V204", false);
+            communicationObject.setItem("V301", false);
+            communicationObject.setItem("V401", false);
+
+            // Phase 4
+            communicationObject.setItem("V204", true);
+            Thread.Sleep(1000);  // TODO: Sleep(Td), asetuksista? Vai ei asetuksista?
+            
+            // Phase 5
+            communicationObject.setItem("V204", false);
+
+            stage = Stage.black_liquor_fill;
+
+        }
+        private void BlackLiquorFill()
+        {
+
+        }
+        private void WhiteLiquorFill()
+        {
+
+        }
+        private void Cooking()
+        {
+
+        }
+        private void Discharge()
+        {
+
         }
     }
 }
