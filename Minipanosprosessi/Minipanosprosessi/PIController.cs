89cd39eb764c4c8dd560f759e91dd19f66878349
kuusnies <70370638+kuusnies@users.noreply.github.com>
2022-12-14 20:56:58 +0200

    Luotu PIController-luokka

---
+++ Minipanosprosessi/Minipanosprosessi/PIController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minipanosprosessi
+{
+    class PIController
+    {
+        double Kp;
+        double Ki;
+        double integrator;
+        double setpoint;
+        double Ts;
+
+        public PIController(double kp, double ki, double ts)
+        {
+            Kp = kp;
+            Ki = ki;
+            Ts = ts;
+            integrator = 0;
+            setpoint = 0;
+        }
+
+        public void setSetpoint(double newSetpoint)
+        {
+            setpoint = newSetpoint;
+        }
+
+        public double updateOutput(double measuredValue)
+        {
+            double result = 0;
+            double error = 0;
+
+            error = setpoint - measuredValue;
+
+            integrator += (Ki * error * Ts);
+
+            result = Kp * error + integrator;
+
+            return result;
+        }
+    }
+}
