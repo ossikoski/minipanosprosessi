b6b5c40b5b5a18930c413253a4f6c8a52fb70d8c
ossikoski <ossioskari@gmail.com>
2022-12-15 13:21:48 +0200

    Lisätty UnitTestProject

---
+++ Minipanosprosessi/UnitTestProject/TestControlSystem.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Minipanosprosessi;
+using Moq;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TestControlSystem
+    {
+        ControlSystem controlSystem;
+        public class MockMainWindow
+        {
+            private object lockObject;
+            public MockMainWindow()
+            {
+                lockObject = new object();
+            }
+        }
+        public class MockCommunication
+        {
+            private object lockObject;
+            public MockCommunication()
+            {
+                lockObject = new object();
+
+            }
+        }
+        public TestControlSystem()
+        {
+            var mockMainWindow = Mock.Of<MainWindow>();
+            var mockCommunication = Mock.Of<Communication>();
+            controlSystem = new ControlSystem(mockCommunication, mockMainWindow);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            controlSystem.Start();
+        }
+    }
+}
